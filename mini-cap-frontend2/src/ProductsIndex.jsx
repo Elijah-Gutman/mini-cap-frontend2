bdc25e2738ad4b3350789fb459a74f3911ba8db1
Elijah Gutman <gutmane7@gmail.com>
2024-11-25 11:02:34 -0500

    setting up json queries with index action

---
+++ mini-cap-frontend2/src/ProductsIndex.jsx
@@ -0,0 +1,19 @@
+export function ProductsIndex({ products }) {
+  return (
+    <div>
+      <h1>All products ({products.length} total)</h1>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <img src={product.primary_image_url} />
+          <p>Price: {product.price}</p>
+          <p>Description: {product.description}</p>
+          <p>Tax: {product.tax}</p>
+          <p>Total Price: {product.total}</p>
+          {<p>Supplier: {product.supplier.name}</p>}
+        </div>
+      ))}
+    </div>
+  );
+}
